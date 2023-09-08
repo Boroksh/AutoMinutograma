4923262862ef0e1eb14f1dbb88395cd71ae596b9
Jorge Alexander Rocha <37847832+Boroksh@users.noreply.github.com>
2023-09-08 05:27:02 -0500

    Add files via upload

--- AutoMinutograma.py
+++ AutoMinutograma.py
@@ -5,33 +5,47 @@ from selenium.webdriver.common.action_chains import ActionChains
 import selenium
 import schedule
 import time
-usuario1 ="nnnn"
-password1 = "nnnn"
+
+usuario1 = "nnnn"
+password1 = "nnn"
+
+
 def ejecutar_tarea():
     print("Versión de Selenium:", selenium.__version__)
-    options = webdriver.FirefoxOptions()
-    options.binary_location = "C:/Users/nnn/Desktop/Autominutograma/geckodriver.exe"
-    driver = webdriver.Firefox(options=options)
-    
+    options = webdriver.ChromeOptions()
+    #options.add_argument("--headless")  # ejecutar Chrome en no visible
+    driver = webdriver.Chrome(options=options)
+
     driver.maximize_window()
     driver.get("http://172.24.43.27:8008/Semaforo_Claro/index.php")
     driver.implicitly_wait(30)
-    
+
     usuario = driver.find_element(By.ID, 'user')
     usuario.send_keys(usuario1)
-    
+
     password = driver.find_element(By.ID, 'password')
     password.send_keys(password1)
-    
+
     login_button = driver.find_element(By.XPATH, '//*[@id="wrapper"]/div/div/div/div[1]/div/form/input')
     ActionChains(driver).move_to_element(login_button).click().perform()
-    
+
     driver.implicitly_wait(30)
-    
+
+    Marcar = driver.find_element(By.XPATH, '//*[@id="sidebar-nav"]/div/div[1]/nav/ul/li[3]/a/span')
+    ActionChains(driver).move_to_element(Marcar).click().perform()
+
+    driver.implicitly_wait(30)
+
+    Marcar = driver.find_element(By.XPATH, '//*[@id="subPages2"]/ul/li[1]/a')
+    ActionChains(driver).move_to_element(Marcar).click().perform()
+
+    driver.implicitly_wait(30)
+
     Marcar = driver.find_element(By.XPATH, '//*[@id="wrapper"]/div[2]/div/div/div/div/div/form/center/form/center/input')
-    ActionChains(driver).move_to_element(login_button).click().perform()
-    
-    print("Inicio de sesión exitoso")
+    ActionChains(driver).move_to_element(Marcar).click().perform()
+
+    driver.implicitly_wait(30)
+    print("Proceso se realizo correctamente")
     driver.quit()
 
 
